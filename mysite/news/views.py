7ea3f2a0bc23133f770aa0362f82790771fe897b
Яна Русина <yana@MacBook-Air-Ana.local>
2023-08-18 19:09:49 +0300

    Initial commit

---
+++ mysite/news/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import News
+
+
+def index(request):
+    news = News.objects.all()
+    context = {
+        'news': news,
+        'title': 'Список новостей'
+    }
+
+    return render(request, 'news/index.html', context=context)
+
+def test(request):
+    return HttpResponse('<h1>Test page</h1>')
